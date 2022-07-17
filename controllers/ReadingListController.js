6cebb290260fcb27e953b3fb77e8cddc83053470
Uhmopt <qethum@gmail.com>
2022-07-17 14:48:29 -0700

    [new] default data

--- controllers/ReadingListController.js
+++ controllers/ReadingListController.js
@@ -12,12 +12,6 @@ const formatRow = (row = {}) =>
       }
     : row;
 
-const DEFAULT_TABLE_DATA = [
-  { id: 1, title: "Book Title 1", author: "Author 1" },
-  { id: 2, title: "Book Title 2", author: "Author 2" },
-  { id: 3, title: "Book Title 3", author: "Author 3" },
-];
-
 const FILE_PATH = "/db/database.json";
 
 class ReadingListController {
