6452da19f10a2974aa8b9143534e6298471397f7
Uhmopt <qethum@gmail.com>
2022-07-17 14:49:46 -0700

    [New] router

--- routes/routes/reading-list.js
+++ routes/routes/reading-list.js
@@ -1,8 +1,8 @@
 const readingListController = require("../../controllers/ReadingListController");
 
 module.exports = function (router) {
-  router.get("/box", readingListController.get);
-  router.post("/box/create", readingListController.create);
-  router.post("/box/update", readingListController.update);
-  router.get("/box/delete/:id", readingListController.delete);
+  router.get("/reading-list", readingListController.get);
+  router.post("/reading-list/create", readingListController.create);
+  router.post("/reading-list/update", readingListController.update);
+  router.get("/reading-list/delete/:id", readingListController.delete);
 };
